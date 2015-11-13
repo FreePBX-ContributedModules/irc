3c72bb7c95fe1bf4aa138d355061fc27b1006738
Andrew Nagy <andrew.nagy@the159.com>
2015-11-13 14:49:46 -0800

    FREEPBX-10438

--- page.irc.php
+++ page.irc.php
@@ -75,12 +75,12 @@ function startirc(element) {
 	nick = getCookie('ircnick');
 	if (nick == null) nick = '';
 	nick = prompt("<?php echo _("What nickname would you like to use? If you leave this blank, a nick will be automatically generated for you.")?>", nick);
-	if ((nick == null) || (nick == '')) {
+	if ((nick != null) || (nick == '')) {
 		win = window.open("config.php?type=tool&display=<?php echo urlencode($display)?>&action=startreal&quietmode=yes", "IRC Support", "width=660, height=480, location=no, menubar=no, status=no, toolbar=no, scrollbars=no");
 		win.resizeTo(660, 480);
 		win.focus();
 		return true;
-	} else {
+	} else if(nick != '' && nick != null) {
 		var expiry = new Date();
 		expiry.setTime(expiry.getTime() + 60 * 60 * 24 * 30); // 30 days
 		setCookie('ircnick', nick, expiry);
@@ -104,10 +104,6 @@ if (isset($_GET['nick'])) {
 if (!$quietmode) {
 ?>
 
-<div class="rnav"><ul>
-    <li><a href="config.php?type=tool&display=<?php echo urlencode($display)?>&action=start" onclick="startirc(this);"><?php echo _("Start IRC")?></a></li>
-    <li><a href="http://www.freepbx.org/support-and-professional-services" target="_docs"><?php echo _("Online Resources")?></a></li>
-</ul></div>
 <h2>
 <?php echo _("Online Support")?>
 </h2>
@@ -155,11 +151,12 @@ switchLinks(document);
 		// Do IRC stuff
 	break;
 	case "":
+  $brand = \FreePBX::Config()->get("DASHBOARD_FREEPBX_BRAND");
 ?>
 
-<?php echo _("This allows you to contact the FreePBX channel on IRC."); ?>
+<?php echo sprintf(_("This allows you to contact the %s channel on IRC."),$brand); ?>
 
-<?php echo _("As IRC is an un-moderated international medium, FreePBX, Schmooze Com, Inc or any other party can not be held responsible for the actions or behavior of other people on the network"); ?>
+<?php echo sprintf(_("As IRC is an un-moderated international medium, %s, Sangoma, Inc or any other party can not be held responsible for the actions or behavior of other people on the network"),$brand); ?>
 
 <?php echo _("When you connect to IRC, to assist in support, the IRC client will automatically send the following information to everyone in the #freepbx channel:"); ?>
 
@@ -167,10 +164,28 @@ switchLinks(document);
 <li> <?php echo _("Your Linux Distribution:");
            $ver=irc_getversioninfo();
            echo " ($ver)"; ?>
-<li> <?php echo _("Your FreePBX version:");
+<li> <?php echo sprintf(_("Your %s version:"),$brand);
            $ver=getversion();
            echo " (".$ver.")"; ?>
 </ul>
-<?php echo _("If you do not want this information to be made public, please use another IRC client, or contact a commercial support provider");
-break;
+<?php echo _("If you do not want this information to be made public, please use another IRC client, or contact a commercial support provider");?>
+</br>
+</br>
+<h3><?php echo _("The top 2 Rules:")?></h2>
+<ol>
+  <li><?php echo _("Insults, name-calling, harassment of any form, vulgarity, profanity, obscenity, etc. are not allowed. See below for more information.")?></li>
+  <li><?php echo _("Be patient. Sometimes no one will see your question for quite a while if they are away from the computer or busy elsewhere. Sometimes people are chatting but no one answers your question. Just ask your question and hang out in the channel until you see some activity or find someone willing to assist you. Do not ask every two minutes. If no-one answers you may ask again 15 to 20 minutes later.")?></li>
+</ol>
+<?php echo sprintf(_("If you do not agree these rules, please feel free not to use any of the %s's IRC channels."),$brand)?>
+<br/>
+</br>
+<input id="agree" type="checkbox"> <label for="agree"><?php echo _("I agree to the rules and terms above")?></label></br>
+<button id="launch" class="btn" onclick="startirc(this);" disabled><?php echo _("Start IRC")?></button>
+<a class="btn" href="http://www.freepbx.org/support-and-professional-services" target="_docs"><?php echo _("Online Resources")?></a>
+<script>
+  $("#agree").change(function() {
+    $("#launch").prop("disabled",!$("#agree").is(":checked"));
+  });
+</script>
+<?php break;
 }
